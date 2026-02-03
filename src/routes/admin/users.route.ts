ed1ac9147657724bc33ed8859c4e1f3dc830899a
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2026-02-03 20:25:46 +0545

    feat(admin): add admin users routes

---
+++ src/routes/admin/users.route.ts
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import { requireAuth } from "../../middlewares/auth.middleware";
+import { adminOnly } from "../../middlewares/admin/adminOnly.middleware";
+import { userImageUpload } from "../../middlewares/userUpload.middleware";
+
+import {
+  createUserAdmin,
+  deleteUserAdmin,
+  getUserByIdAdmin,
+  getUsersAdmin,
+  updateUserAdmin,
+} from "../../controllers/admin/users.controller";
+
+const router = Router();
+
+// 🔒 Protect all admin routes
+router.use(requireAuth, adminOnly);
+
+// POST /api/admin/users
+router.post("/users", userImageUpload, createUserAdmin);
+
+// GET /api/admin/users
+router.get("/users", getUsersAdmin);
+
+// GET /api/admin/users/:id
+router.get("/users/:id", getUserByIdAdmin);
+
+// PUT /api/admin/users/:id
+router.put("/users/:id", userImageUpload, updateUserAdmin);
+
+// DELETE /api/admin/users/:id
+router.delete("/users/:id", deleteUserAdmin);
+
+export default router;
